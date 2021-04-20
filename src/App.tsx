dc74d7c21cb19208bdf8decc8ec5b31cd014bbe0
lijianru <2219269374@qq.com>
2021-04-20 17:00:21 +0800

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { ROUTERS } from './constants/router';
+
+function App() {
+  return (
+    <Router>
+      <Switch>
+        {ROUTERS.map(({ path, component }) => (
+          <Route exact key={path} path={path} component={component} />
+        ))}
+      </Switch>
+    </Router>
+  );
+}
+
+export default App;
